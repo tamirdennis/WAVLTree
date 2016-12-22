92fb99af6e6fd5b5f5974b73fa9c21ee9e5666fb
Rotem Tzaban <rotemtzaban@localhost.localdomain>
2016-12-22 20:37:23 +0200

    first commit

---
+++ src/WAVLTree.java
@@ -0,0 +1,143 @@
+/**
+ *
+ * WAVLTree
+ *
+ * An implementation of a WAVL Tree with
+ * distinct integer keys and info
+ *
+ */
+
+public class WAVLTree {
+	private WAVLNode root;
+	private int size;
+	private WAVLNode min;
+	private WAVLNode max;
+  /**
+   * public boolean empty()
+   *
+   * returns true if and only if the tree is empty
+   *
+   */
+  public boolean empty() {
+    return size == 0; // to be replaced by student code
+  }
+
+ /**
+   * public String search(int k)
+   *
+   * returns the info of an item with key k if it exists in the tree
+   * otherwise, returns null
+   */
+  public String search(int k)
+  {
+	return "42";  // to be replaced by student code
+  }
+
+  /**
+   * public int insert(int k, String i)
+   *
+   * inserts an item with key k and info i to the WAVL tree.
+   * the tree must remain valid (keep its invariants).
+   * returns the number of rebalancing operations, or 0 if no rebalancing operations were necessary.
+   * returns -1 if an item with key k already exists in the tree.
+   */
+   public int insert(int k, String i) {
+	  return 42;	// to be replaced by student code
+   }
+
+  /**
+   * public int delete(int k)
+   *
+   * deletes an item with key k from the binary tree, if it is there;
+   * the tree must remain valid (keep its invariants).
+   * returns the number of rebalancing operations, or 0 if no rebalancing operations were needed.
+   * returns -1 if an item with key k was not found in the tree.
+   */
+   public int delete(int k)
+   {
+	   return 42;	// to be replaced by student code
+   }
+
+   /**
+    * public String min()
+    *
+    * Returns the info of the item with the smallest key in the tree,
+    * or null if the tree is empty
+    */
+   public String min()
+   {
+	   return min.info; // to be replaced by student code
+   }
+
+   /**
+    * public String max()
+    *
+    * Returns the info of the item with the largest key in the tree,
+    * or null if the tree is empty
+    */
+   public String max()
+   {
+	   return max.info; // to be replaced by student code
+   }
+
+  /**
+   * public int[] keysToArray()
+   *
+   * Returns a sorted array which contains all keys in the tree,
+   * or an empty array if the tree is empty.
+   */
+  public int[] keysToArray()
+  {
+        int[] arr = new int[42]; // to be replaced by student code
+        return arr;              // to be replaced by student code
+  }
+
+  /**
+   * public String[] infoToArray()
+   *
+   * Returns an array which contains all info in the tree,
+   * sorted by their respective keys,
+   * or an empty array if the tree is empty.
+   */
+  public String[] infoToArray()
+  {
+        String[] arr = new String[42]; // to be replaced by student code
+        return arr;                    // to be replaced by student code
+  }
+
+   /**
+    * public int size()
+    *
+    * Returns the number of nodes in the tree.
+    *
+    * precondition: none
+    * postcondition: none
+    */
+   public int size()
+   {
+	   return size; // to be replaced by student code
+   }
+
+  /**
+   * public class WAVLNode
+   *
+   * If you wish to implement classes other than WAVLTree
+   * (for example WAVLNode), do it in this file, not in 
+   * another file.
+   * This is an example which can be deleted if no such classes are necessary.
+   */
+   private class WAVLNodeBase{
+	   public int rank = -1;
+   }
+   
+  private class WAVLNode extends WAVLNodeBase{
+	  public WAVLNode parent;
+	  public WAVLNode left;
+	  public WAVLNode right;
+	  public int key;
+	  public String info;
+  }
+
+}
+  
+
